b0f35cd91a13a72bd0bcfc17192c5a623d0563b9
mayurpatil888 <mayur@ost.com>
2018-11-09 15:31:49 +0530

    expose index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+* KYC Javascript SDK
+*
+* @module index
+*
+* */
+
+
+const rootPrefix = '.';
+
+const KYCSDK = function () {
+  const oThis = this;
+
+};
+
+module.exports = KYCSDK;
